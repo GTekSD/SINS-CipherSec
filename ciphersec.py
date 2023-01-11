7b0d9614151d9d6a750dc95152ee7ed46ca6dae4
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-01-11 22:53:27 +0530

    Update ciphersec.py

--- ciphersec.py
+++ ciphersec.py
@@ -24,31 +24,48 @@ else:
 # Module of regular expression is used with search()
 import re
 import mmap
-password = input("Enter password:- ")
+import pyfiglet
+
+# import pyfiglet module
+result = pyfiglet.figlet_format("SINS CipherSec", font = "future"  )
+print(result)
+
+password = input("Enter password to check:- ")
 flag = 0
+
+# Passwords should contain three of the four character types:
 while True:
 	if (len(password)<=8):
 		flag = -1
 		break
+		
+	# Lowercase letters: a-z
 	elif not re.search("[a-z]", password):
 		flag = -1
 		break
+		
+	# Uppercase letters: A-Z	
 	elif not re.search("[A-Z]", password):
 		flag = -1
 		break
+		
+	# Numbers: 0-9
 	elif not re.search("[0-9]", password):
 		flag = -1
 		break
-	elif not re.search("[_@$]" , password):
+		
+	# Symbols: ~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/
+	elif not re.search("~`!@#$%^&*()_-+={[}]|\:;'\"<,>.?/" , password):
 		flag = -1
 		break
+		
 	elif re.search("\s" , password):
 		flag = -1
 		break
 	else:
 		flag = 0
 		print("Valid Password")
-		with open(r'/home/manoj/Desktop/rockyou.txt', 'rb', 0) as file:
+		with open(r'/usr/share/wordlists/rockyou.txt', 'rb', 0) as file:
                      s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
                      if s.find(b'password') != -1:
                         print('compromised')
