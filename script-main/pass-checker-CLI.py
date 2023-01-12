e2cc05027cd025400460355aadd1200d29137d30
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-01-12 21:51:04 +0530

    Update pass-checker-CLI.py

--- script-main/pass-checker-CLI.py
+++ script-main/pass-checker-CLI.py
@@ -1,23 +1,39 @@
+# script for password checker CLI mode
+
+import os
 import re
 
-# function to check the password
 def check_password(password):
+    
     # check if the password is at least 8 characters long
     if len(password) < 8:
         return "Password must be at least 8 characters long."
+    
     # check if the password contains at least one numeric character
     elif not any(i.isdigit() for i in password):
         return "Password must contain at least one numeric character."
+    
     # check if the password contains at least one special character
     elif not any(i in "!@#$%^&*()_+-=[]{};:'\"\\|,.<>/?" for i in password):
         return "Password must contain at least one special character."
-    # check if the password contains any consecutive repeating characters
-    elif re.search(r"(\w)\1", password):
-        return "Password cannot contain any consecutive repeating characters."
+    
+    # check if the password contains more than 2 consecutive repeating characters
+    elif re.search(r"(\w)\1{2,}", password):
+        return "Password can contain only 2 consecutive repeating characters."
+    
+    # check if the password is in any of the .txt files in the directory
+    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'db-SecLists')
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            with open(os.path.join(directory, filename)) as file:
+                if password in file.read():
+                    return "Breached Password. The Password you entered is in the compromised database. Please use difficult another password"
+    
     # if the password pass all the above conditions, then return valid
     else:
-        return "Password is valid."
+        return "Secure password."
 
 password = input("Enter the password: ")
 result = check_password(password)
 print(result)
+
