2cb59275e825357409f5281c337b6312a1d3f606
Mohamed Ibrahim <97780329+anafareez@users.noreply.github.com>
2023-01-12 17:36:04 +0530

    Update pass-checker-GUI.py

--- script-main/pass-checker-GUI.py
+++ script-main/pass-checker-GUI.py
@@ -16,34 +16,29 @@ def check_password(password):
     # check if the password contains any consecutive repeating characters
     elif re.search(r"(\w)\1", password):
         return "Password cannot contain any consecutive repeating characters."
+    # check if the password is in the list of passwords
+    elif not any(password.startswith(line[0]) and password in line for line in open('passwords.txt').read().splitlines()):
+        return "Breached Password. The Password you entered is in the compromised database. Please use difficult another password"
     # if the password pass all the above conditions, then return valid
     else:
-        return "Password is valid."
+        return "Secure password."
 
-# function to execute when the submit button is clicked
 def on_submit():
-    # get the password from the text input field
     password = entry.get()
-    # check the password
     result = check_password(password)
-    # display a message box with the result
     messagebox.showinfo("Password Checker", result)
 
-# create the root window
 root = tk.Tk()
+root.geometry("300x400")
 root.title("Password Checker")
 
-# create a label for the text input field
 label = tk.Label(root, text="Enter password:")
 label.pack()
 
-# create the text input field
-entry = tk.Entry(root, show="*")
+entry = tk.Entry(root)
 entry.pack()
 
-# create the submit button
 submit = tk.Button(root, text="Submit", command=on_submit)
 submit.pack()
 
-# start the GUI event loop
 root.mainloop()
