7e13c9ab51fce2f65f413dfd5362ec3a91a31d6d
Mohamed Ibrahim <97780329+anafareez@users.noreply.github.com>
2023-01-12 15:17:01 +0530

    Create generator.py

---
+++ beta/generator.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def generate_password():
+    name = name_entry.get()
+    passphrase = passphrase_entry.get()
+    password = ""
+    j = 0
+    for i in range(len(passphrase)):
+        char = passphrase[i]
+        if j == len(name):
+            j = 0
+        x = (ord(char) + ord(name[j])) % 26
+        x += ord('A')
+        password += chr(x)
+        j += 1
+    password_label.config(text=password)
+    
+def save_password():
+    password = password_label.cget("text")
+    file = open("password.txt", "w")
+    file.write(password)
+    file.close()
+    messagebox.showinfo("Success", "Password saved to password.txt.")
+
+def copy_password():
+    password = password_label.cget("text")
+    root.clipboard_clear()
+    root.clipboard_append(password)
+    messagebox.showinfo("Success", "Password copied to clipboard.")
+
+root = tk.Tk()
+root.geometry("200x200")
+root.title("Password Generator")
+
+name_label = tk.Label(root, text="Enter Your Name:")
+name_label.pack()
+
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+passphrase_label = tk.Label(root, text="Enter Your Passphrase:")
+passphrase_label.pack()
+
+passphrase_entry = tk.Entry(root)
+passphrase_entry.pack()
+
+generate_button = tk.Button(root, text="Generate Password", command=generate_password)
+generate_button.pack()
+
+password_label = tk.Label(root)
+password_label.pack()
+
+save_button = tk.Button(root, text="Save Password", command=save_password)
+save_button.pack()
+
+copy_button = tk.Button(root, text="Copy Password", command=copy_password)
+copy_button.pack()
+
+root.mainloop()
