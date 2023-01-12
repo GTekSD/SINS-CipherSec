9642a39f6596403fa703d0af1733095454d6717c
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-01-12 21:08:54 +0530

    Update pass-checker-GUI.py

--- script-main/pass-checker-GUI.py
+++ script-main/pass-checker-GUI.py
@@ -7,15 +7,47 @@ def check_password(password):
     # check if the password is at least 8 characters long
     if len(password) < 8:
         return "Password must be at least 8 characters long."
+    
+    # check if the password contains at least one numeric character
+    elif not any(i.isdigit() for i in password):
+        return "Password must contain at least one numeric character."
+    
+    # check if the password contains at least one special character
+    elif not any(i in "!@#$%^&*()_+-=[]{};:'\"\\|,.<>/?" for i in password):
+        return "Password must contain at least one special character."
+    
+    # check if the password contains any consecutive repeating characters
+    elif re.search(r"(\w)\1", password):
+        return "Password cannot contain any consecutive repeating characters."
+    
+    # check if the password is in any of the .txt files in the directory
+    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'db-SecLists')
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            with open(os.path.join(directory, filename)) as file:
+                if password in file.read():
+                    return "Breached Password. The Password you entered is in the compromised database. Please use difficult another password"
+    
+    # if the password pass all the above conditions, then return valid
+    else:
+        return "Secure password."
+    
+""" NEW UPDATED
+import re
+
+def check_password(password):
+    # check if the password is at least 8 characters long
+    if len(password) < 8:
+        return "Password must be at least 8 characters long."
     # check if the password contains at least one numeric character
     elif not any(i.isdigit() for i in password):
         return "Password must contain at least one numeric character."
     # check if the password contains at least one special character
     elif not any(i in "!@#$%^&*()_+-=[]{};:'\"\\|,.<>/?" for i in password):
         return "Password must contain at least one special character."
-    # check if the password contains any consecutive repeating characters
-    elif re.search(r"(\w)\1", password):
-        return "Password cannot contain any consecutive repeating characters."
+    # check if the password contains more than 2 consecutive repeating characters
+    elif re.search(r"(\w)\1{2,}", password):
+        return "Password can contain only 2 consecutive repeating characters."
     # check if the password is in any of the .txt files in the directory
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'db-SecLists')
     for filename in os.listdir(directory):
@@ -27,6 +59,8 @@ def check_password(password):
     else:
         return "Secure password."
 
+"""
+  
 
 def on_submit():
     password = entry.get()
