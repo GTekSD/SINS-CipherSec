5b05f7a4d934612f0498b3f167a99d7ac0336af0
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-01-13 00:28:34 +0530

    Update pass-gen-CLI.py

--- script-main/pass-gen-CLI.py
+++ script-main/pass-gen-CLI.py
@@ -1,3 +1,52 @@
+# Script for generating password CLI mode
+
+import os
+from datetime import datetime
+
+def generate_password():
+    # Get the name and passphrase entered by the user
+    name = input("Enter Your Name: ")
+    passphrase = input("Enter Your Passphrase: ")
+
+    password = ""
+    j = 0
+    # Generate the password using the name and passphrase
+    for i in range(len(passphrase)):
+        char = passphrase[i]
+        if j == len(name):
+            j = 0
+        x = (ord(char) + ord(name[j])) % 26
+        x += ord('A')
+        password += chr(x)
+        j += 1
+    # Print the generated password
+    print(password)
+    return password
+
+def save_password(password):
+    # Get the current date
+    date = datetime.now().strftime("%Y-%m-%d")
+    # Create the user directory with the name entered by the user
+    user_directory = "users/" + name_entry.get()
+    # Check if the directory already exists
+    if not os.path.exists(user_directory):
+        os.mkdir(user_directory)
+    else:
+        # Rename the previous password file as old_password_<current_date>.txt
+        os.rename(user_directory+"/password_"+date+".txt",user_directory+"/old_password_"+date+".txt")
+    # Save the new password in the user directory with the current date
+    file = open(user_directory+"/password_" + date + ".txt", "w")
+    file.write(password)
+    file.close()
+    # Show a message with the location of the saved password
+    print("Password saved to "+user_directory+"/password_" + date + ".txt")
+
+password = generate_password()
+save_password(password)
+
+
+"""
+
 import sys #importing sys module to exit the program
 import getpass #importing getpass module to hide the entered passphrase
 import pyperclip # importing pyperclip module to copy the password to clipboard
@@ -40,3 +89,4 @@ def generate_password():
 
 # calling the function to execute the code
 generate_password()
+"""
