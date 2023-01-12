8021dedc74dd1e82adceeedb6e536749c277cfb1
Mohamed Ibrahim <97780329+anafareez@users.noreply.github.com>
2023-01-12 15:34:27 +0530

    Update PassGeneratorCLI.py

--- beta/PassGeneratorCLI.py
+++ beta/PassGeneratorCLI.py
@@ -1,20 +1,27 @@
-import sys
-import getpass
+import sys #importing sys module to exit the program
+import getpass #importing getpass module to hide the entered passphrase
+import pyperclip # importing pyperclip module to copy the password to clipboard
 
 def generate_password():
+    #prompting user for name
     name = input("Enter Your Name:")
+    #prompting user for passphrase and hiding it
     passphrase = getpass.getpass("Enter Your Passphrase:")
     password = ""
     j = 0
     for i in range(len(passphrase)):
         char = passphrase[i]
+        #looping the name if passphrase is longer
         if j == len(name):
             j = 0
+        #generating password by adding ascii value of name and passphrase
         x = (ord(char) + ord(name[j])) % 26
         x += ord('A')
         password += chr(x)
         j += 1
+    #printing the generated password
     print("Generated Password:", password)
+    #prompting user to save the password
     save = input("Do you want to save the password in password.txt file? (y/n)")
     if save.lower() == 'y':
         try:
@@ -23,12 +30,13 @@ def generate_password():
             print("Password saved to password.txt.")
         except:
             print("Error occured while saving password")
+    #prompting user to copy the password to clipboard
     copy = input("Do you want to copy the password to clipboard? (y/n)")
     if copy.lower() == 'y':
-        import pyperclip
         pyperclip.copy(password)
         print("Password copied to clipboard.")
     else:
         sys.exit()
 
+# calling the function to execute the code
 generate_password()
