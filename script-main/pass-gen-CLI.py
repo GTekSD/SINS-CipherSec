99ed45dab230e95c6635b5c9ab74b7028c16d69d
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-03-25 02:46:20 +0530

    Update pass-gen-CLI.py
    Change log for CLI version:
    
    Original script created for generating password based on name and passphrase.
    Updated script to generate password based on username, platform, and pin.
    Password generation now includes upper and lower case letters, digits, and symbols. Ambiguous characters are excluded.
    Generated password is now always 24 characters long.
    Password is automatically copied to clipboard.
    Password is no longer saved in any directory.

--- script-main/pass-gen-CLI.py
+++ script-main/pass-gen-CLI.py
@@ -1,50 +1,57 @@
-# Script for generating password CLI mode
+# Script for generating password CLI mode | v25/3/2023
 
-import os
-import datetime
+import string
+import random
+import hashlib
+import pyperclip
 
 # function to generate password
 def generate_password():
-    name = input("Enter Your Name: ")
-    passphrase = input("Enter Your Passphrase: ")
-    password = ""
-    j = 0
-    for i in range(len(passphrase)):
-        char = passphrase[i]
-        if j == len(name):
-            j = 0
-        x = (ord(char) + ord(name[j])) % 26
-        x += ord('A')
-        password += chr(x)
-        j += 1
-    print("Generated Password: ", password)
-    return password,name
+    # get user input
+    username = input("Enter your username: ")
+    platform = input("Enter the platform name: ")
+    pin = input("Enter your PIN: ")
 
-# function to save password
-def save_password(password,name):
-    #get the current date
-    now = datetime.datetime.now()
-    current_date = now.strftime("%Y-%m-%d")
-    # create the directory "users" if it does not exist
-    if not os.path.exists("users"):
-        os.makedirs("users")
-    # create a directory with the user's name if it does not exist
-    user_directory = "users/" + name
-    if not os.path.exists(user_directory):
-        os.makedirs(user_directory)
-    # rename the previous password file to "old.password" if it exists
-    if os.path.exists(user_directory+'/password.txt'):
-        os.rename(user_directory+'/password.txt', user_directory+'/old.password')
-    # save the new password to a file with the current date as the file name
-    file = open(user_directory + "/password"+current_date+".txt", "w")
-    file.write(password)
-    file.close()
-    print("Password saved to "+user_directory+"/password"+current_date+".txt.")
+    # generate hash value from input values
+    input_str = f"{username}{platform}{pin}"
+    hash_obj = hashlib.sha256(input_str.encode())
+    hash_value = int.from_bytes(hash_obj.digest(), byteorder='big')
+
+    # use hash value to seed random number generator
+    random.seed(hash_value)
+
+    # define character sets to use for generating password
+    uppercase_letters = string.ascii_uppercase
+    lowercase_letters = string.ascii_lowercase
+    digits = string.digits
+    symbols = "!@#$%^&*()_+-=[]{};':\"\\|,.<>/?"
+
+    # exclude ambiguous characters
+    ambiguous_chars = "B8G6I1l0OQDS5Z2"
+    for char in ambiguous_chars:
+        uppercase_letters = uppercase_letters.replace(char, "")
+        lowercase_letters = lowercase_letters.replace(char.lower(), "")
+        digits = digits.replace(char, "")
+        symbols = symbols.replace(char, "")
+
+    # combine character sets
+    char_set = uppercase_letters + lowercase_letters + digits + symbols
+
+    # generate password
+    password = ""
+    while len(password) < 24:
+        password += random.choice(char_set)
+
+    # copy password to clipboard
+    pyperclip.copy(password)
+
+    # print generated password
+    print("Generated Password: ", password)
+    return password
 
 # main function to call other functions
 def main():
-    password,name = generate_password()
-    save_password(password,name)
+    password = generate_password()
 
 # call the main function
 if __name__ == '__main__':
